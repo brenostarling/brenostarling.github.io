c43f715eddab20ee07e5b6d76af3c74a8080feab
brenostarling <brenostarling1@hotmail.com>
2023-05-25 11:25:12 -0300

    scroll to top btn hide/show

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 // import Acomplishments from '../components/Acomplishments/Acomplishments';
+import { useState, useEffect } from 'react';
 import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
 import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
@@ -9,10 +10,23 @@ import { FaArrowUp } from 'react-icons/fa';
 import { Section, FloatingButton } from '../styles/GlobalComponents';
 
 const Home = () => {
+  const [showButton, setShowButton] = useState(false);
+
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
+  const handleScroll = () => {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    setShowButton(scrollTop > 0);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <Layout>
@@ -24,7 +38,7 @@ const Home = () => {
       <Technologies />
       <Timeline />
       {/* <Acomplishments /> */}
-      <FloatingButton onClick={scrollToTop}><FaArrowUp /></FloatingButton>
+      {showButton && (<FloatingButton onClick={scrollToTop}> <FaArrowUp /> </FloatingButton>)}
     </Layout>
   );
 };

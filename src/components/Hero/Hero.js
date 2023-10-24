bc5402accb4889fcd3608176fa8a8feb1bfa62d5
brenostarling <brenostarling1@hotmail.com>
2023-10-24 12:57:33 -0300

    update description

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -11,7 +11,7 @@ const Hero = (props) => (
           Hey there,
         </SectionTitle>
         <SectionText>
-          I'm Breno Starling, a Fullstack Developer from Betim, MG - Brazil with over two years of experience in Fullstack development. I have expertise in Asterisk, SIP phone system, and WebRTC. Currently, I am proficient in developing frontend applications using React, Vue2, Vue3, and AngularJS, while utilizing Node.js for backend development.
+          I'm Breno Starling, a Fullstack Developer from Betim, MG - Brazil with over three years of experience in Fullstack development. I have expertise in Asterisk, SIP phone system, and WebRTC. Currently, I am proficient in developing frontend applications using React, Vue2, Vue3, AngularJS and Angular2+, while utilizing Node.js for backend development.
         </SectionText>
         <div style={{ display: 'flex', gap: '10px' }}>
           <Button onClick={() => window.open('/files/resume.pdf', '_blank')}>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -33,14 +33,14 @@ export default class MyDocument extends Document {
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
           <meta name="title" content="Breno Starling - Fullstack Developer" />
-          <meta name="description" content="I'm Breno Starling, a Fullstack Developer from Betim, MG - Brazil with over two years of experience in Fullstack development. I have expertise in Asterisk, SIP phone system, and WebRTC. Currently, I am proficient in developing frontend applications using React, Vue2, Vue3, and AngularJS, while utilizing Node.js for backend development." />
+          <meta name="description" content="I'm Breno Starling, a Fullstack Developer from Betim, MG - Brazil with over three years of experience in Fullstack development. I have expertise in Asterisk, SIP phone system, and WebRTC. Currently, I am proficient in developing frontend applications using React, Vue2, Vue3, AngularJS and Angular2+, while utilizing Node.js for backend development." />
           <meta property="og:type" content="website" />
           <meta property="og:title" content="Breno Starling - Fullstack Developer" />
-          <meta property="og:description" content="I'm Breno Starling, a Fullstack Developer from Betim, MG - Brazil with over two years of experience in Fullstack development. I have expertise in Asterisk, SIP phone system, and WebRTC. Currently, I am proficient in developing frontend applications using React, Vue2, Vue3, and AngularJS, while utilizing Node.js for backend development." />
+          <meta property="og:description" content="I'm Breno Starling, a Fullstack Developer from Betim, MG - Brazil with over three years of experience in Fullstack development. I have expertise in Asterisk, SIP phone system, and WebRTC. Currently, I am proficient in developing frontend applications using React, Vue2, Vue3, AngularJS and Angular2+, while utilizing Node.js for backend development." />
           <meta property="og:image" content="/images/banner_breno.png" />
           <meta property="twitter:card" content="summary_large_image" />
           <meta property="twitter:title" content="Breno Starling - Fullstack Developer" />
-          <meta property="twitter:description" content="I'm Breno Starling, a Fullstack Developer from Betim, MG - Brazil with over two years of experience in Fullstack development. I have expertise in Asterisk, SIP phone system, and WebRTC. Currently, I am proficient in developing frontend applications using React, Vue2, Vue3, and AngularJS, while utilizing Node.js for backend development." />
+          <meta property="twitter:description" content="I'm Breno Starling, a Fullstack Developer from Betim, MG - Brazil with over three years of experience in Fullstack development. I have expertise in Asterisk, SIP phone system, and WebRTC. Currently, I am proficient in developing frontend applications using React, Vue2, Vue3, AngularJS and Angular2+, while utilizing Node.js for backend development." />
           <meta property="twitter:image" content="/images/banner_breno.png" />
         </Head>
         <body>
